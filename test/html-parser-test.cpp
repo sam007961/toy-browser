a219e1cca8983eef99b92aca17a73672550470e0
Samer Itani <iamsameritani@gmail.com>
2020-04-02 20:44:57 +0300

    Added empty tests and configured cmake for running tests.

---
+++ test/html-parser-test.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+#include <html-parser.hpp>
+
+TEST(htmlParserTest, test1) {
+    EXPECT_EQ(true, true);
+}
---
+++ test/tokenizer-test.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+#include <parser.hpp>
+
+TEST(tokenizerTest, test1) {
+    EXPECT_EQ(true, true);
+}
