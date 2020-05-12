66e745e23a3e59d4b2c0354f57dc0414f1996662
Samer Itani <iamsameritani@gmail.com>
2020-05-12 03:00:37 +0300

    Implemented layout nodes.

--- src/layout.cpp
+++ src/layout.cpp
@@ -1,2 +1,21 @@
 #include <vector>
 #include <layout.hpp>
+
+layout::LayoutBox::LayoutBox(style::StyledNode* style_node)
+    : style_node(style_node) {}
+
+
+layout::AnnonymousBlock::AnnonymousBlock() : LayoutBox(nullptr) {}
+
+layout::LayoutBox* layout::AnnonymousBlock::get_inline_container() { return this; }
+
+
+layout::LayoutBox* layout::InlineNode::get_inline_container() { return this; }
+
+
+layout::LayoutBox* layout::BlockNode::get_inline_container() {
+    if(auto ptr = dynamic_cast<AnnonymousBlock*>(children.back().get()); !ptr) {
+        children.push_back(std::make_unique<AnnonymousBlock>());
+    }
+    return children.back().get();
+}
--- src/layout.hpp
+++ src/layout.hpp
@@ -5,12 +5,12 @@
 
 namespace layout {
     struct Rect {
-        float x, y;
-        float width, heigth;
+        float x = 0, y = 0;
+        float width = 0, heigth = 0;
     };
 
     struct EdgeSizes {
-        float left, right, top, bottom;
+        float left = 0, right = 0, top = 0, bottom = 0;
     };
 
     struct Dimensions {
@@ -24,15 +24,27 @@ namespace layout {
     typedef std::unique_ptr<LayoutBox> LayoutBoxPtr;
 
     struct LayoutBox {
+        const style::StyledNode* const style_node;
         Dimensions dimensions;
         std::vector<LayoutBoxPtr> children;
 
-        virtual style::StyledNode* get_styled_node() const = 0;
-        virtual LayoutBoxPtr get_inline_container() const = 0;
+        LayoutBox(style::StyledNode* styled_node = nullptr);
+        virtual LayoutBox* get_inline_container() = 0;
     };
 
     struct AnnonymousBlock : public LayoutBox {
-        virtual style::StyledNode* get_styled_node() const;
-        virtual LayoutBoxPtr get_inline_container() const;
+        AnnonymousBlock();
+        AnnonymousBlock(style::StyledNode*) = delete;
+        virtual LayoutBox* get_inline_container();
     };
+
+    struct InlineNode : public LayoutBox {
+        virtual LayoutBox* get_inline_container();
+    };
+
+    struct BlockNode : public LayoutBox {
+        virtual LayoutBox* get_inline_container();
+    };
+
+    LayoutBoxPtr build_layout_tree(const style::StyledNode& style_node);
 }
